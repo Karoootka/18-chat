1cd2a2cd43656ab66425defcf1429d3f8f9edb9f
Karolina Madej <kkarolinamadej@gmail.com>
2018-07-16 15:27:36 +0200

    Install webpack, babel, react dependecies

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = (env) => {
+  return {
+    mode: env,
+    entry: (env !== 'production' ? [
+          'react-hot-loader/patch',
+          'webpack-dev-server/client?http://localhost:8080',
+          'webpack/hot/only-dev-server',
+        ] : []).concat(['./client/index.js']),
+    output: {
+      filename: './bundle.js',
+      path: path.resolve(__dirname, 'public'),
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          loader: "babel-loader",
+          options: {
+            plugins: env != 'production' ? ["react-hot-loader/babel"] : []
+          }
+        },
+        {
+          test: /\.css$/,
+          use: [
+            { loader: 'style-loader'},
+            {
+                loader: 'css-loader',
+                options: {
+                  modules: true
+                }
+            }
+          ]
+        }
+      ]
+    },
+    plugins: [new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      filename: 'index.html',
+      inject: 'body'
+    })]
+  }
+};
